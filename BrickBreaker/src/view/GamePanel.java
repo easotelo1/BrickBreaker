7d4c6f5b95ce44d9087b6a8e9b29371e269fbda1
Everett Sotelo <easotelo1@gmail.com>
2025-08-15 02:01:46 -0700

    added initial paddle object

--- BrickBreaker/src/view/GamePanel.java
+++ BrickBreaker/src/view/GamePanel.java
@@ -1,9 +1,11 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+import model.Paddle;
 import model.Screen;
 
 @SuppressWarnings("serial")
@@ -12,7 +14,17 @@ public class GamePanel extends JPanel {
 	private int[] mainPanelSize;
 	private String currResolution;
 	
+	private Paddle paddle;
+	
 	public GamePanel() {
+		mainPanelSize = screen.getScreenResolution();
+
 		setBackground(Color.BLACK);
+		
+		paddle = new Paddle(mainPanelSize[0]/2, mainPanelSize[1]/2, 130, 25); //TODO: Just trying to get a simple drawing right. worry about threading later
+	}
+	
+	public void paintComponent(Graphics g) {
+		g.fillRect(paddle.getX(), paddle.getY(), paddle.getWidth(), paddle.getHeight());
 	}
 }
